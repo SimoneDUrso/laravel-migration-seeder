29514a5fc0a7077d0eb1a04b0bc3d412dfcca888
SimoneDUrso <simone.durso98@gmail.com>
2024-09-26 17:13:32 +0200

    [UPD] Exercise and Bonus completed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,26 +4,73 @@
     <div class="container mt-5">
         <div class="row g-3">
             @foreach ($trains as $train)
-                <div class="col-3">
+                <div class="col-4">
                     <div class="card">
                         <div class="card-body">
-                            <ul class="list-unstyled">
-                                <li><strong>Nome Azienda:</strong> {{ $train->azienda }}</li>
-                                <li><strong>Stazione di Partenza:</strong> {{ $train->stazione_di_partenza }}</li>
-                                <li><strong>Stazione di Arrivo:</strong> {{ $train->stazione_di_arrivo }}</li>
+                            <table class="table table-striped">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Nome Azienda</th>
+                                        <td>{{ $train->azienda }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Stazione di Partenza</th>
+                                        <td>{{ $train->stazione_di_partenza }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Stazione di Arrivo</th>
+                                        <td>{{ $train->stazione_di_arrivo }}</td>
+                                    </tr>
 
-                                @if ($train->orario_di_partenza > $train->orario_di_arrivo)
-                                    <li><strong>Orario di Partenza:</strong>Mi dispiace, sei arrivato tardi! Il treno è già
-                                        partito.</li>
-                                @else
-                                    <li><strong>Orario di Partenza:</strong> {{ $train->orario_di_partenza }}</li>
-                                @endif
-                                <li><strong>Orario di Arrivo:</strong> {{ $train->orario_di_arrivo }}</li>
-                                <li><strong>Codice Treno:</strong> {{ $train->codice_treno }}</li>
-                                <li><strong>Numero Carrozze:</strong> {{ $train->numero_carrozze }}</li>
-                                <li><strong>In orario:</strong> {{ $train->in_orario }}</li>
-                                <li><strong>Cancellato:</strong> {{ $train->cancellato }}</li>
-                            </ul>
+                                    {{-- Condizione per vedere se l'orario di partenza è maggiore rispetto all'orario di arrivo --}}
+                                    @if ($train->orario_di_partenza > $train->orario_di_arrivo)
+                                        <tr>
+                                            <th scope="row">Orario di Partenza</th>
+                                            <td>Mi dispiace, sei arrivato tardi! Il treno è già partito.</td>
+                                        </tr>
+                                    @else
+                                        <tr>
+                                            <th scope="row">Orario di Partenza</th>
+                                            <td>{{ $train->orario_di_partenza }}</td>
+                                        </tr>
+                                    @endif
+                                    <tr>
+                                        <th scope="row">Orario di Arrivo</th>
+                                        <td>{{ $train->orario_di_arrivo }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Codice Treno</th>
+                                        <td>{{ $train->codice_treno }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Numero Carrozze</th>
+                                        <td>{{ $train->numero_carrozze }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">In Orario</th>
+                                        <td>
+                                            {{-- Condizione che verifica se la corsa è stata cancellata. In questo caso il campo IN ORARIO si svuota --}}
+                                            @if ($train->cancellato == 0)
+                                                @if ($train->in_orario == 1)
+                                                    Si
+                                                @else
+                                                    No
+                                                @endif
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Cancellato</th>
+                                        <td>
+                                            @if ($train->cancellato == 1)
+                                                Si
+                                            @else
+                                                No
+                                            @endif
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
