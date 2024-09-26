9d2ddb4ff38bb1d74112c1168006957a91a75bbf
SimoneDUrso <simone.durso98@gmail.com>
2024-09-26 15:45:56 +0200

    [UPD] Created columns inside trains table

--- database/migrations/2024_09_26_133321_create_trains_table.php
+++ database/migrations/2024_09_26_133321_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno', 50);
+            $table->tinyinteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
